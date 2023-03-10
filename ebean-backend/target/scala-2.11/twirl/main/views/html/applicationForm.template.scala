151a3c91ec7df6669d29ae1e0102c2ccc79c8ff0
SMUEric1127 <85500156+SMUEric1127@users.noreply.github.com>
2023-03-10 15:59:49 -0600

    Fix spacing

--- ebean-backend/target/scala-2.11/twirl/main/views/html/applicationForm.template.scala
+++ ebean-backend/target/scala-2.11/twirl/main/views/html/applicationForm.template.scala
@@ -89,54 +89,55 @@ Seq[Any](format.raw/*4.84*/("""
     <div class="row">
       <div class="input-field col s12">
         <label for="coursesTaken">Courses taken before:</label>
+          <br>
         <select multiple id="coursesTaken" name="coursesTaken[]" required>
-        """),_display_(/*60.10*/for(position <- positions) yield /*60.36*/ {_display_(Seq[Any](format.raw/*60.38*/("""
-          """),format.raw/*61.11*/("""<option value=""""),_display_(/*61.27*/position/*61.35*/.position_id),format.raw/*61.47*/("""">"""),_display_(/*61.50*/position/*61.58*/.courseTitle),format.raw/*61.70*/("""</option>
-        """)))}),format.raw/*62.10*/("""
-        """),format.raw/*63.9*/("""</select>
+        """),_display_(/*61.10*/for(position <- positions) yield /*61.36*/ {_display_(Seq[Any](format.raw/*61.38*/("""
+          """),format.raw/*62.11*/("""<option value=""""),_display_(/*62.27*/position/*62.35*/.position_id),format.raw/*62.47*/("""">"""),_display_(/*62.50*/position/*62.58*/.courseTitle),format.raw/*62.70*/("""</option>
+        """)))}),format.raw/*63.10*/("""
+        """),format.raw/*64.9*/("""</select>
       </div>
     </div>
     <div class="row">
       <div class="input-field col s12">
         <label for="preference1">1st Preference to be TA for:</label>
+          <br>
         <select name="preference1" required>
-          <option value="" disabled selected>Select a course</option>
-          """),_display_(/*71.12*/for(position <- positions) yield /*71.38*/ {_display_(Seq[Any](format.raw/*71.40*/("""
-            """),format.raw/*72.13*/("""<option value=""""),_display_(/*72.29*/position/*72.37*/.position_id),format.raw/*72.49*/("""">"""),_display_(/*72.52*/position/*72.60*/.courseTitle),format.raw/*72.72*/("""</option>
-          """)))}),format.raw/*73.12*/("""
-        """),format.raw/*74.9*/("""</select>
+          """),_display_(/*72.12*/for(position <- positions) yield /*72.38*/ {_display_(Seq[Any](format.raw/*72.40*/("""
+            """),format.raw/*73.13*/("""<option value=""""),_display_(/*73.29*/position/*73.37*/.position_id),format.raw/*73.49*/("""">"""),_display_(/*73.52*/position/*73.60*/.courseTitle),format.raw/*73.72*/("""</option>
+          """)))}),format.raw/*74.12*/("""
+        """),format.raw/*75.9*/("""</select>
       </div>
     </div>
     <div class="row">
       <div class="input-field col s12">
         <label for="preference2">2nd Preference to be TA for:</label>
+          <br>
         <select name="preference2">
-          <option value="" disabled selected>Select a course</option>
-          """),_display_(/*82.12*/for(position <- positions) yield /*82.38*/ {_display_(Seq[Any](format.raw/*82.40*/("""
-            """),format.raw/*83.13*/("""<option value=""""),_display_(/*83.29*/position/*83.37*/.position_id),format.raw/*83.49*/("""">"""),_display_(/*83.52*/position/*83.60*/.courseTitle),format.raw/*83.72*/("""</option>
-          """)))}),format.raw/*84.12*/("""
-          """),format.raw/*85.11*/("""</select>
+          """),_display_(/*83.12*/for(position <- positions) yield /*83.38*/ {_display_(Seq[Any](format.raw/*83.40*/("""
+            """),format.raw/*84.13*/("""<option value=""""),_display_(/*84.29*/position/*84.37*/.position_id),format.raw/*84.49*/("""">"""),_display_(/*84.52*/position/*84.60*/.courseTitle),format.raw/*84.72*/("""</option>
+          """)))}),format.raw/*85.12*/("""
+          """),format.raw/*86.11*/("""</select>
         </div>
     </div>
     <div class="row">
       <div class="input-field col s12">
         <label for="preference3">3rd Preference to be TA for:</label>
+          <br>
         <select name="preference3">
-          <option value="" disabled selected>Select a course</option>
-          """),_display_(/*93.12*/for(position <- positions) yield /*93.38*/ {_display_(Seq[Any](format.raw/*93.40*/("""
-            """),format.raw/*94.13*/("""<option value=""""),_display_(/*94.29*/position/*94.37*/.position_id),format.raw/*94.49*/("""">"""),_display_(/*94.52*/position/*94.60*/.courseTitle),format.raw/*94.72*/("""</option>
-          """)))}),format.raw/*95.12*/("""
-        """),format.raw/*96.9*/("""</select>
+          """),_display_(/*94.12*/for(position <- positions) yield /*94.38*/ {_display_(Seq[Any](format.raw/*94.40*/("""
+            """),format.raw/*95.13*/("""<option value=""""),_display_(/*95.29*/position/*95.37*/.position_id),format.raw/*95.49*/("""">"""),_display_(/*95.52*/position/*95.60*/.courseTitle),format.raw/*95.72*/("""</option>
+          """)))}),format.raw/*96.12*/("""
+        """),format.raw/*97.9*/("""</select>
       </div>
     </div>
     <div class="row">
       <div class="input-field col s12">
         <label for="coursesNotPreferred">Courses Not Preferred:</label>
         <select multiple id="coursesNotPreferred" name="coursesNotPreferred">
-        """),_display_(/*103.10*/for(position <- positions) yield /*103.36*/ {_display_(Seq[Any](format.raw/*103.38*/("""
-          """),format.raw/*104.11*/("""<option value=""""),_display_(/*104.27*/position/*104.35*/.position_id),format.raw/*104.47*/("""">"""),_display_(/*104.50*/position/*104.58*/.courseTitle),format.raw/*104.70*/("""</option>
-        """)))}),format.raw/*105.10*/("""
-        """),format.raw/*106.9*/("""</select>
+        """),_display_(/*104.10*/for(position <- positions) yield /*104.36*/ {_display_(Seq[Any](format.raw/*104.38*/("""
+          """),format.raw/*105.11*/("""<option value=""""),_display_(/*105.27*/position/*105.35*/.position_id),format.raw/*105.47*/("""">"""),_display_(/*105.50*/position/*105.58*/.courseTitle),format.raw/*105.70*/("""</option>
+        """)))}),format.raw/*106.10*/("""
+        """),format.raw/*107.9*/("""</select>
       </div>
   </div>
   <div class="row">
@@ -150,9 +151,9 @@ Seq[Any](format.raw/*4.84*/("""
       <button id="submit-app-button" type="submit" name="action">Submit</button>
     </div>
   </div>
-    <script>$(document).ready(function()"""),format.raw/*120.41*/("""{"""),format.raw/*120.42*/("""
-      """),format.raw/*121.7*/("""$('select').formSelect();
-    """),format.raw/*122.5*/("""}"""),format.raw/*122.6*/(""");</script>
+    <script>$(document).ready(function()"""),format.raw/*121.41*/("""{"""),format.raw/*121.42*/("""
+      """),format.raw/*122.7*/("""$('select').formSelect();
+    """),format.raw/*123.5*/("""}"""),format.raw/*123.6*/(""");</script>
   """)))}))
       }
     }
@@ -174,11 +175,11 @@ Seq[Any](format.raw/*4.84*/("""
 object applicationForm extends applicationForm_Scope0.applicationForm_Scope1.applicationForm
               /*
                   -- GENERATED --
-                  DATE: Fri Mar 10 15:40:50 CST 2023
+                  DATE: Fri Mar 10 15:52:35 CST 2023
                   SOURCE: /Users/ericvudeptrai/Desktop/Sprint1/Repo/Remake-Scott-Lab/ebean-backend/app/views/applicationForm.scala.html
-                  HASH: 6028f0cdc68ac71bf89b1d1d7b0c243e94069922
-                  MATRIX: 945->108|1122->190|1152->195|1166->201|1260->287|1299->289|1330->294|1941->877|1970->878|2015->895|2083->935|2112->936|2349->1146|2362->1150|2393->1160|2598->1338|2611->1342|2641->1351|2867->1550|2880->1554|2907->1560|3123->1749|3136->1753|3163->1759|3417->1986|3430->1990|3462->2001|3699->2211|3712->2215|3751->2232|4038->2492|4051->2496|4092->2515|4365->2761|4407->2787|4447->2789|4486->2800|4529->2816|4546->2824|4579->2836|4609->2839|4626->2847|4659->2859|4709->2878|4745->2887|5064->3179|5106->3205|5146->3207|5187->3220|5230->3236|5247->3244|5280->3256|5310->3259|5327->3267|5360->3279|5412->3300|5448->3309|5758->3592|5800->3618|5840->3620|5881->3633|5924->3649|5941->3657|5974->3669|6004->3672|6021->3680|6054->3692|6106->3713|6145->3724|6457->4009|6499->4035|6539->4037|6580->4050|6623->4066|6640->4074|6673->4086|6703->4089|6720->4097|6753->4109|6805->4130|6841->4139|7124->4394|7167->4420|7208->4422|7248->4433|7292->4449|7310->4457|7344->4469|7375->4472|7393->4480|7427->4492|7478->4511|7515->4520|7952->4928|7982->4929|8017->4936|8075->4966|8104->4967
-                  LINES: 32->4|37->4|39->6|39->6|39->6|39->6|40->7|48->15|48->15|49->16|50->17|50->17|56->23|56->23|56->23|60->27|60->27|60->27|66->33|66->33|66->33|70->37|70->37|70->37|76->43|76->43|76->43|80->47|80->47|80->47|86->53|86->53|86->53|93->60|93->60|93->60|94->61|94->61|94->61|94->61|94->61|94->61|94->61|95->62|96->63|104->71|104->71|104->71|105->72|105->72|105->72|105->72|105->72|105->72|105->72|106->73|107->74|115->82|115->82|115->82|116->83|116->83|116->83|116->83|116->83|116->83|116->83|117->84|118->85|126->93|126->93|126->93|127->94|127->94|127->94|127->94|127->94|127->94|127->94|128->95|129->96|136->103|136->103|136->103|137->104|137->104|137->104|137->104|137->104|137->104|137->104|138->105|139->106|153->120|153->120|154->121|155->122|155->122
+                  HASH: 07a510830dd5441b719d9b904390db8dea1527b1
+                  MATRIX: 945->108|1122->190|1152->195|1166->201|1260->287|1299->289|1330->294|1941->877|1970->878|2015->895|2083->935|2112->936|2349->1146|2362->1150|2393->1160|2598->1338|2611->1342|2641->1351|2867->1550|2880->1554|2907->1560|3123->1749|3136->1753|3163->1759|3417->1986|3430->1990|3462->2001|3699->2211|3712->2215|3751->2232|4038->2492|4051->2496|4092->2515|4380->2776|4422->2802|4462->2804|4501->2815|4544->2831|4561->2839|4594->2851|4624->2854|4641->2862|4674->2874|4724->2893|4760->2902|5024->3139|5066->3165|5106->3167|5147->3180|5190->3196|5207->3204|5240->3216|5270->3219|5287->3227|5320->3239|5372->3260|5408->3269|5663->3497|5705->3523|5745->3525|5786->3538|5829->3554|5846->3562|5879->3574|5909->3577|5926->3585|5959->3597|6011->3618|6050->3629|6307->3859|6349->3885|6389->3887|6430->3900|6473->3916|6490->3924|6523->3936|6553->3939|6570->3947|6603->3959|6655->3980|6691->3989|6974->4244|7017->4270|7058->4272|7098->4283|7142->4299|7160->4307|7194->4319|7225->4322|7243->4330|7277->4342|7328->4361|7365->4370|7802->4778|7832->4779|7867->4786|7925->4816|7954->4817
+                  LINES: 32->4|37->4|39->6|39->6|39->6|39->6|40->7|48->15|48->15|49->16|50->17|50->17|56->23|56->23|56->23|60->27|60->27|60->27|66->33|66->33|66->33|70->37|70->37|70->37|76->43|76->43|76->43|80->47|80->47|80->47|86->53|86->53|86->53|94->61|94->61|94->61|95->62|95->62|95->62|95->62|95->62|95->62|95->62|96->63|97->64|105->72|105->72|105->72|106->73|106->73|106->73|106->73|106->73|106->73|106->73|107->74|108->75|116->83|116->83|116->83|117->84|117->84|117->84|117->84|117->84|117->84|117->84|118->85|119->86|127->94|127->94|127->94|128->95|128->95|128->95|128->95|128->95|128->95|128->95|129->96|130->97|137->104|137->104|137->104|138->105|138->105|138->105|138->105|138->105|138->105|138->105|139->106|140->107|154->121|154->121|155->122|156->123|156->123
                   -- GENERATED --
               */
           
